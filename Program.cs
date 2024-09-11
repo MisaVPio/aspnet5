cce0e4dcaffabbfde9aea338cd2d6bcc13154f46
MisaVPio <misavpio@outlook.com>
2024-09-11 09:57:06 -0300

    add files

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Win32.SafeHandles;
+
+namespace ASP.NET 
+{
+    class Program
+    {
+
+        // CURSO ASP.NET 5 - RICARDO MAROQUIO
+        // AULA 1 - 26:57m
+        static void Main(string[] args)
+        
+        {
+            var servidorHttp = new ServidorHttp();
+        }
+    }
+}
---
+++ ServidorHttp.cs
@@ -0,0 +1,74 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class ServidorHttp
+{
+    private TcpListener Controlador {get; set;} = null!;
+    private int Porta {get;set;}
+
+    private int QtdRequests{get;set;}
+
+    public ServidorHttp(int porta = 8080)
+    {
+        this.Porta = porta;
+
+        try{
+            this.Controlador = new TcpListener(IPAddress.Parse("127.0.0.1"),this.Porta);
+            this.Controlador.Start();
+            Console.WriteLine($"Servidor HTTP está rodando na porta {this.Porta}");
+            Console.WriteLine($"Para acessar,  digite no navegador: http://localhost:{this.Porta}");
+            Task servidorHttpTask = Task.Run(() => AguardarRequests());
+            servidorHttpTask.GetAwaiter().GetResult();    
+
+       }
+        catch(Exception e)
+        {
+            Console.WriteLine($"Erro ao iniciar servidor na porta {this.Porta}:\n{e.Message}");
+        }
+
+    }
+
+
+    private async Task AguardarRequests()
+    {
+        while(true)
+        {
+            Socket conexao = await this.Controlador.AcceptSocketAsync();
+            this.QtdRequests++;
+            Task task = Task.Run(() => ProcessarRequest(conexao, this.QtdRequests));
+
+        }
+
+    }
+
+    private void ProcessarRequest(Socket conexao, int numeroRequest)
+    {
+        Console.WriteLine($"Processando request #{numeroRequest} ...\n");
+        if(conexao.Connected)
+        {
+            byte[] bytesRequisicao = new byte[1024];
+            conexao.Receive(bytesRequisicao, bytesRequisicao.Length, 0);
+            string textoRequisicao = Encoding.UTF8.GetString(bytesRequisicao)
+            .Replace((char)0, ' ').Trim();
+            if(textoRequisicao.Length > 0)
+            {
+                Console.WriteLine($"\n{textoRequisicao}\n");
+                var bytesCabecalho = GerarCabecalho("HTTP/1.1", "text/html;charset=utf-8","200",0);
+                int bytesEnviados = conexao.Send(bytesCabecalho, bytesCabecalho.Length, 0);
+                conexao.Close();
+                Console.WriteLine($"\n{bytesEnviados} bytes enviados em resposta à requisição #{numeroRequest}.");
+            }
+        }
+        Console.WriteLine($"\nRequest {numeroRequest} finalizado");
+    }
+
+    public byte[] GerarCabecalho(string versaoHttp, string tipoMime, string codigoHttp, int qtdeBytes = 0){
+        StringBuilder texto = new StringBuilder();
+        texto.Append($"{versaoHttp} {codigoHttp} {Environment.NewLine}");
+        texto.Append($"Server: Servidor Http Simples 1.0{Environment.NewLine}");
+        texto.Append($"Content-Type: {tipoMime} {Environment.NewLine}");
+        texto.Append($"Content-Length: {qtdeBytes}{Environment.NewLine}{Environment.NewLine}");
+        return Encoding.UTF8.GetBytes(texto.ToString());
+    }
+}
